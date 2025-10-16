cef02be4bfe7dbbe362b3cf3c840288fd2113e7b
chigoziri1 <anihdavid497@gmail.com>
2025-10-16 09:47:46 +0100

    commit initial files

---
+++ score-board.js
@@ -0,0 +1,13 @@
+let render1 = document.getElementById("span1-el");
+let render2 = document.getElementById("span2-el");
+
+let count = 0;
+let count2 = 0;
+
+function add(num) {
+	render1.textContent =	count += num;
+}
+
+function addTwo(num) {
+	render2.textContent =	count2 += num;
+}
